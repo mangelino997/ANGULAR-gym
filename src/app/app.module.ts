be31104e184bc12cb465450a8b1655cd0150f280
Blas Bravo <blas@draimo.com>
2018-12-26 08:23:18 -0300

    Se agregó librerías y url base, se creó carpetas componentes, modulos y servicios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,47 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule, MatDividerModule,
+  MatSelectModule, MatTabsModule, MatIconModule, MatCardModule, MatSidenavModule,
+  MatAutocompleteModule, MatInputModule, MatRadioModule, MatTableModule, MatDialogModule,
+  MatProgressBarModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatRadioModule,
+    MatTableModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    HttpModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servicios/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  //Define la url base del servicio web
+  public URL_BASE: string = 'http://localhost:8082/lesionws/auth'; //192.168.0.99
+  //Constructor
+  constructor() {}
+  //Obtiene la url base
+  public getUrlBase(): string {
+    return this.URL_BASE;
+  }
+}
